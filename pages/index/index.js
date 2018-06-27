aa72d36f993aed0a94c7badf40c9c541ba1df533
avaqi29 <avaqi29@users.noreply.github.com>
2018-06-27 17:00:11 -0400

    switch pics and pull down refresh

--- pages/index/index.js
+++ pages/index/index.js
@@ -11,37 +11,63 @@ const weatherMap = {
   'snow': '雪'
 }
 
+const weatherColorMap = {
+  'sunny': '#cbeefd',
+  'cloudy': '#deeef6',
+  'overcast': '#c6ced2',
+  'lightrain': '#bdd5e1',
+  'heavyrain': '#c5ccd0',
+  'snow': '#aae1fc'
+}
+
 Page({
   data: {
-    wxml_temperature: '14°',
-    wxml_weather: '阴天'
+    wxml_temperature: '',
+    wxml_weather: '',
+    weather_pic : ""
   },
 
   onLoad(){
-    console.log("hello world")
-    var that = this;
-    wx.request({
-      url: 'https://test-miniprogram.com/api/weather/now', 
-      data: {
-        city: '上海市',
-      },
-      
-      success: res=> {
-        console.log(res.data)
-        let result = res.data.result
-        let temperature = result.now.temp
-        let weather = result.now.weather
-        console.log(temperature, weather)
-        
-        this.setData({
-          wxml_temperature: temperature + '°',
-          wxml_weather: weatherMap[weather] 
-        })
-      }
-
-
+    this.getNow()
+  },
+  
+  onPullDownRefresh(){
+    this.getNow(() => {
+      wx.stopPullDownRefresh() //need considering removal
     })
 
+  },
 
-  }
+getNow(callback){
+  console.log("hello world")
+  var that = this;
+  wx.request({
+    url: 'https://test-miniprogram.com/api/weather/now',
+    data: {
+      city: '北京市',
+    },
+
+    success: res => {
+      console.log(res.data)
+      let result = res.data.result
+      let temperature = result.now.temp
+      let weather = result.now.weather
+      console.log(temperature, weather)
+
+      this.setData({
+        wxml_temperature: temperature + '°',
+        wxml_weather: weatherMap[weather],
+        weather_pic: "/pics/" + weather + "-bg.png"
+      })
+      wx.setNavigationBarColor({
+        frontColor: '#000000',
+        backgroundColor: weatherColorMap[weather],
+      })
+    },
+    complete: () => {
+      wx.stopPullDownRefresh()
+    }
+  })
+
+}
 })
