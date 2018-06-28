0a123e6d0b7dafa19dfdab3ff37d7a2cc877ff10
avaqi29 <avaqi29@users.noreply.github.com>
2018-06-28 01:47:30 -0400

    complete main page

--- pages/index/index.js
+++ pages/index/index.js
@@ -24,7 +24,8 @@ Page({
   data: {
     wxml_temperature: '',
     wxml_weather: '',
-    weather_pic : ""
+    weather_pic : "",
+    forecast: []
   },
 
   onLoad(){
@@ -38,36 +39,58 @@ Page({
 
   },
 
-getNow(callback){
-  console.log("hello world")
-  var that = this;
-  wx.request({
-    url: 'https://test-miniprogram.com/api/weather/now',
-    data: {
-      city: '北京市',
-    },
+  getNow(callback){
+    //console.log("hello world")
+    //var that = this;
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data: {
+        city: '北京市',
+      },
 
-    success: res => {
-      console.log(res.data)
-      let result = res.data.result
-      let temperature = result.now.temp
-      let weather = result.now.weather
-      console.log(temperature, weather)
+      success: res => {
+        //console.log(res.data)
+        let result = res.data.result
+        this.getMainView(result)
+        //set forecast
+        this.getListView(result)
+      },
+      complete: () => {
+        wx.stopPullDownRefresh()
+      }
+    })
+  },
 
-      this.setData({
-        wxml_temperature: temperature + '°',
-        wxml_weather: weatherMap[weather],
-        weather_pic: "/pics/" + weather + "-bg.png"
+  getMainView(result){
+    let temperature = result.now.temp
+    let weather = result.now.weather
+    console.log(temperature, weather)
+
+    this.setData({
+      wxml_temperature: temperature + '°',
+      wxml_weather: weatherMap[weather],
+      weather_pic: "/pics/" + weather + "-bg.png"
+    })
+    wx.setNavigationBarColor({
+      frontColor: '#000000',
+      backgroundColor: weatherColorMap[weather],
+    })
+  },
+
+  getListView(result){
+    let getForecast = result.forecast
+    let forecast = []
+    let nowHour = new Date().getHours()
+    for (let i = 0; i < 24; i += 3) {
+      forecast.push({
+        time: (i + nowHour) % 24 + "时",
+        iconPath: '/pics/' + getForecast[i / 3].weather + '-icon.png',
+        temp: getForecast[i / 3].temp + "°"
       })
-      wx.setNavigationBarColor({
-        frontColor: '#000000',
-        backgroundColor: weatherColorMap[weather],
-      })
-    },
-    complete: () => {
-      wx.stopPullDownRefresh()
-    }
-  })
-
-}
+      }
+    forecast[0].time = '现在'
+    this.setData({
+      forecast: forecast
+    })
+  }
 })
